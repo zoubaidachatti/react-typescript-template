9d153eafee73b795f07ae28a580aec231346c064
Malek Mneri <mneri.malek@gmail.com>
2024-03-21 15:32:18 +0100

    theme-control: hide selector after picking theme

--- src/components/ThemeSelector/ThemeSelector.tsx
+++ src/components/ThemeSelector/ThemeSelector.tsx
@@ -7,6 +7,11 @@ export default function ThemeSelector() {
   const [clickedTheme, setClickedTheme] = useState<AppTheme>();
   const [showModePicker, setShowModePicker] = useState<boolean>(false);
   const [toggleTheme, setToggleTheme] = useState<boolean>(false);
+
+  const setTheme = (_theme?: unknown) => {
+    setToggleTheme(false);
+    setShowModePicker(false);
+  };
   return (
     <>
       <Stack direction="row" alignItems={'center'} marginLeft={'auto'} spacing={1}>
@@ -37,19 +42,23 @@ export default function ThemeSelector() {
               ))}
             </Stack>
             <Collapse in={showModePicker}>
-              <Stack direction="row" spacing={1}>
-                <Stack
-                  height={'40px'}
-                  width={'135px'}
-                  borderRadius={'5px'}
-                  bgcolor={clickedTheme?.lightTheme.palette?.primary?.light}
-                ></Stack>
-                <Stack
-                  height={'40px'}
-                  width={'135px'}
-                  borderRadius={'5px'}
-                  bgcolor={clickedTheme?.lightTheme.palette?.primary?.dark}
-                ></Stack>
+              <Stack direction="row" spacing={1.2}>
+                <Tooltip title="light" onClick={() => setTheme()}>
+                  <Stack
+                    height={'40px'}
+                    width={'135px'}
+                    borderRadius={'5px'}
+                    bgcolor={clickedTheme?.lightTheme.palette?.primary?.light}
+                  ></Stack>
+                </Tooltip>
+                <Tooltip title="dark" onClick={() => setTheme()}>
+                  <Stack
+                    height={'40px'}
+                    width={'135px'}
+                    borderRadius={'5px'}
+                    bgcolor={clickedTheme?.lightTheme.palette?.primary?.dark}
+                  ></Stack>
+                </Tooltip>
               </Stack>
             </Collapse>
           </Stack>
